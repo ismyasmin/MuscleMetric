8c2bb0383a6a1290f9575e4517203034d748c729
Yasmin <yasminxismail@gmail.com>
2025-11-18 23:55:46 +0000

    Express App Setup.

---
+++ app.js
@@ -0,0 +1,14 @@
+require('dotenv').config() // invoke config() - going to attach those env vars to the process object
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.json({msg: 'testing'})
+
+
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('listening')
+});
