daa31fbad1b0b988920a2f548a08a0449db19f99
Yasmin <yasminxismail@gmail.com>
2025-11-21 11:25:04 +0000

    Fetch data using backend api. Fetching all workouts & list them on the Home page using useEffect. Added a  proxy: http://localhost:4000 top of package.json to proxy any request that it doesnt recognize to our api server at this address.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,32 @@
+import {useEffect, useState} from 'react'
+
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null) // update workouts using setWorkouts, value of this is gonna be the array of data (const json)
+
+    // shouldnt make this async () => {} 
+    useEffect( () => { // fire when component is rendered
+        const fetchWorkouts = async () => {
+            // fetch workouts from api on the backend
+		        const response = await fetch('/api/workouts')
+
+            // pass json from this response object for something to work with
+            const json = await response.json() // array of objects
+
+            if(response.ok) {
+                setWorkouts(json) // update local states
+            }
+
+        } // fetch the workouts from the api on the backend
+
+        fetchWorkouts()
+    }, [])   // fire it once when it's rendered, not fetch it multiple times every time the component is rendered
+            // empty array  - this is the dependency array & with it being empty, it will only ever fire once the component is rendered
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            {workouts && workouts.map((workout) => ( // if theres a value for workouts, then map through workouts
+                    <p key={workout._id}>{workout.title}</p>
+                ))} {/*normal parentheses not curly braces because returning template*/}
         </div>
     )
 }
